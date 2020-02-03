16eb56042dc8966862915813c838153b527154bd
lmjcbs <hello@liamjacobs.dev>
2020-02-03 12:15:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+const mapToNegativize = array => {
+  let newArray = []
+  array.forEach(element => {
+    newArray.push(-element)
+  });
+  return newArray
+}
+
+const mapToNoChange = array => {
+  let newArray = []
+  array.forEach(element => {
+    newArray.push(element)
+  });
+  return newArray
+}
+
+const mapToDouble = array => {
+  let newArray = []
+  array.forEach(element => {
+    newArray.push(element*2)
+  });
+  return newArray
+}
+
+const mapToSquare = array => {
+  let newArray = []
+  array.forEach(element => {
+    newArray.push(element**2)
+  });
+  return newArray
+}
+
+const reduceToTotal = (array, initial = 0) => {
+  let total = initial
+  array.forEach(element => {
+    total += element
+  });
+  return total
+}
+
+const reduceToAllTrue = array => {
+  let output = true
+  array.forEach(element => {
+    if (!element) {
+      output = false
+    }
+  });
+  return output
+}
+
+const reduceToAnyTrue = array => {
+  let output = false
+  array.forEach(element => {
+    if (!!element) {
+      output = true
+    }
+  });
+  return output
+}
